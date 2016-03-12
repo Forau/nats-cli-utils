e2337f8ae4917edfc42ed45cbdea915da33ca993
Forau <Forau@users.noreply.github.com>
2016-03-12 20:25:40 +0800

    Added nats-tail command

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # nats-cli-utils
-A small collection of command line tools for working with nats messaging system
+A small collection of command line tools for working with nats messaging system.
+This is a work in progress...
+
+Tool        | Description
+----        | -----------
+nats-tail   | Do tail on one or several nats subjects
+
+
+nats-tail
+---------
+`
+Flags:
+      --help           Show context-sensitive help (also try --help-long and --help-man).
+  -u, --uri="nats://localhost:4222"  
+                       Uri to connect with NATS
+  -s, --sub="debug.>"  The subject(s) to listen to
+  -t, --templ="{{time.Unix}}\t{{.Subject}}\t{{.Reply}}\n\t{{.Data | hex }}"  
+                       Template for output in golang text/template format with some added functions
+  -r, --raw            Short for template that just prints the data as it comes. Is equal to -t "{{.Data|printf "%s"}}"
+`
---
+++ cmd/nats-tail/main.go
@@ -0,0 +1,66 @@
+// Copyright (c) 2016 Forau @ github.com. MIT License.
+
+package main
+
+import (
+  "gopkg.in/alecthomas/kingpin.v2"
+  "github.com/nats-io/nats"
+
+  "os"
+  "time"
+  "runtime"
+  "text/template"
+  "encoding/hex"
+)
+
+var (  
+  natsUri = kingpin.Flag("uri", "Uri to connect with NATS").Default(nats.DefaultURL).Short('u').String()
+  sub = kingpin.Flag("sub", "The subject(s) to listen to").Default("debug.>").Short('s').String()
+  templText = kingpin.Flag("templ", "Template for output in golang text/template format with some added functions").
+    Default("{{time.Unix}}\t{{.Subject}}\t{{.Reply}}\n\t{{.Data | hex }}").
+    Short('t').String()
+
+  raw = kingpin.Flag("raw", "Short for template that just prints the data as it comes. Is equal to -t \"{{.Data|printf \"%s\"}}\"").
+    Short('r').Bool()
+  
+  templ *template.Template
+)
+
+func init() {
+  kingpin.Parse()
+
+  funcMap := template.FuncMap{
+		"hex": hex.Dump,
+    "time": time.Now,
+  }
+  tmplText := *templText
+  if *raw {
+    tmplText = "{{.Data|printf \"%s\"}}"
+  }
+  
+  
+  templ = template.Must(template.New("output").Funcs(funcMap).Parse(tmplText))
+}
+
+func printMsg(msg *nats.Msg) {
+  err := templ.Execute(os.Stdout, msg)
+  if err != nil {
+    panic(err)
+  }
+}
+
+func main() {
+  nc, err := nats.Connect(*natsUri)
+  if err != nil {
+    panic(err)
+  }
+
+  _, err = nc.Subscribe(*sub, printMsg)
+  if err != nil {
+    panic(err)
+  }
+  
+  runtime.Goexit()
+}
+
+
